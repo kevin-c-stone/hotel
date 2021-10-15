86fcc62b0ffa8feaf2c3cc09997ca382652c2aa7
Kevin C Stone <kevin.c.stone@outlook.com>
2021-10-15 16:49:18 -0700

    added some css and a form to bookings

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 "use strict";
+
+let fName = document.getElementById("fName");
+let lName = document.getElementById("lName");
+let email = document.getElementById("email");
+let startOfStay = document.getElementById("startOfStay");
+let endOfStay = document.getElementById("endOfStay");
